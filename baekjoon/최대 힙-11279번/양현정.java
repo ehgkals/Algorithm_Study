b6cd82f64e6a460e16e712c6aaf1df3bbd8e3948
yangyanghyunjung <hyunjungyang0922@gmail.com>
2025-10-03 16:39:21 +0900

    양현정_백준-최대힙-11279

---
+++ baekjoon/최대 힙-11279번/양현정.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; ++i) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+                pq.add(x);
+            } else {
+                Integer q = pq.poll();
+                sb.append(q == null ? 0 : q).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+}
