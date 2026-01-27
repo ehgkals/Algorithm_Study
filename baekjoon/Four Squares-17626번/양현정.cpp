645dba4a3f99aec39574780c857b88c912c99c05
yangyanghyunjung <hyunjungyang0922@gmail.com>
2026-01-27 13:40:36 +0900

    양현정 Four Squares 17626- DP 버전

--- baekjoon/Four Squares-17626번/양현정.cpp
+++ baekjoon/Four Squares-17626번/양현정.cpp
@@ -1,51 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n;
-vector<int> v;
-
-int mn = 4;
-void DFS(int L, int num)
-{
-    if (L == 4)
-    {
-        return;
-    }
-
-    int remainder = 0;
-    for (int a : v)
-    {
-        if (mn <= L)
-            continue;
-        remainder = num - a;
-        if (remainder == 0)
-        {
-            if (mn > L)
-                mn = L;
-            return;
-        }
-        DFS(L + 1, remainder);
-    }
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
     cin >> n;
-    int s = 1;
-    while (s * s <= n)
+
+    vector<int> squares;
+    for (int i = 1; i * i <= n; i++)
+        squares.push_back(i * i);
+
+    vector<int> dp(n + 1);
+    for (int i = 1; i <= n; i++)
     {
-        v.push_back(s * s);
-        s++;
+        dp[i] = INT32_MAX;
+        for (int sq : squares)
+        {
+            if (sq > i)
+                break;
+            dp[i] = min(dp[i], dp[i - sq] + 1);
+        }
     }
 
-    DFS(1, n);
-    cout << mn;
-
+    cout << dp[n];
     return 0;
 }
